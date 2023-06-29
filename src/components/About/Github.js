a04b06e8a22826d45b2ff9ed37806cf7f3929e7a
layton <layton.hart.cool@gmail.com>
2023-06-30 00:55:10 +0200

    github user name change

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="marlborocool"
+        username="Fly-Pig00"
         blockSize={15}
         blockMargin={5}
         color="#c084f5"
